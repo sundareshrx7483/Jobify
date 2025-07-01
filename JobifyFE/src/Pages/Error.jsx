d593aa18db4d9e7df2d73cb9dd735104cc6da87f
sundareshrx7483 <sundareshrx7483@gmail.com>
2025-07-01 22:07:36 +0530

    Error Page

--- JobifyFE/src/Pages/Error.jsx
+++ JobifyFE/src/Pages/Error.jsx
@@ -1,4 +1,23 @@
+import { Link, useRouteError } from "react-router-dom"
+
 const Error = () => {
-  return <h1>Error</h1>;
-};
-export default Error;
+  const error = useRouteError()
+  console.log(error)
+  return (
+    <div>
+      <h1>Error Page</h1>
+      
+        {error && (
+        <div>
+          <h6>Status: {error.status}</h6>
+          <h6>Status Text: {error.statusText}</h6>
+          <h6>Data: {error.data}</h6>
+        </div>
+      )}
+      
+      
+      <Link to="/">Back To Home</Link>
+    </div>
+  )
+}
+export default Error
