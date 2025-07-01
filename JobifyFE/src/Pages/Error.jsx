3f4e4b9db0dcc6e6647dbec983c5af76c6c6e4ce
sundareshrx7483 <sundareshrx7483@gmail.com>
2025-07-01 22:13:29 +0530

    Error Page

--- JobifyFE/src/Pages/Error.jsx
+++ JobifyFE/src/Pages/Error.jsx
@@ -2,14 +2,10 @@ import { Link, useRouteError } from "react-router-dom"
 
 const Error = () => {
   const error = useRouteError()
-  console.log(error)
+ 
   return (
     <div>
       <h1>Error Page</h1>
-      
-      
-      
-      
       <Link to="/">Back To Home</Link>
     </div>
   )
