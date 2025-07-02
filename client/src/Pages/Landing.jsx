4df16283db926d25ba1136c198184f69b81f70cd
sundareshrx7483 <sundareshrx7483@gmail.com>
2025-07-02 22:59:53 +0530

    Implemented  landing page

--- client/src/Pages/Landing.jsx
+++ client/src/Pages/Landing.jsx
@@ -1,4 +1,37 @@
+import styled from "styled-components";
+
+import Wrapper from "../assets/wrappers/LandingPage";
+import main from "../assets/images/main.svg";
+import logo from "../assets/images/logo.svg";
+import { Link } from "react-router-dom";
 const Landing = () => {
-  return <h1>Landing</h1>;
+  return (
+    <Wrapper>
+      <nav>
+        <img src={logo} alt="jobify" className="logo" />
+      </nav>
+      <div className="container page">
+        <div className="info">
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            A Job tracking app is a digital tool designed to help users
+            efficiently manage and monitor their work-related tasks, projects,
+            or job applications. It allows individuals or teams to organize
+            assignments, set deadlines, track progress, and receive updates in
+            real-time.
+          </p>
+          <Link to="/register" className="btn register-link">
+            Register
+          </Link>
+          <Link to="/login" className="btn">
+            Login / Demo User
+          </Link>
+        </div>
+        <img src={main} alt="job hunt" className="img main-img" />
+      </div>
+    </Wrapper>
+  );
 };
 export default Landing;
