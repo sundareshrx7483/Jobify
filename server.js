52d1ce8c789ee964f752a214ea4795ae25f10472
sundareshrx7483 <sundareshrx7483@gmail.com>
2025-07-20 23:22:07 +0530

    feat: add server setup with Express and update express dependency

---
+++ server.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("Hello world!");
+});
+
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT} `);
+});
