135f8b77fa44333bf4156c63e407c8f76bc04b41
sundareshrx7483 <sundareshrx7483@gmail.com>
2025-07-22 20:40:55 +0530

    fix: update dotenv and morgan dependencies; refactor server.js for improved readability

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
 import express from "express";
-
+import morgan from "morgan";
+import * as dotenv from "dotenv";
+dotenv.config();
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+app.use(express.json());
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+const port = process.env.PORT || 5100;
 
 app.get("/", (req, res) => {
   res.send("Hello world!");
 });
 
-app.listen(PORT, () => {
-  console.log(`server is running on port ${PORT} `);
+app.post("/", (req, res) => {
+  console.log(req);
+  res.json({ message: "Data Recieved", data: req.body });
+});
+
+app.listen(port, () => {
+  console.log(`server is running on port ${port} `);
 });
