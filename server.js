db451b0890396fee158014674a37b0229eebc877
sundareshrx7483 <sundareshrx7483@gmail.com>
2025-07-23 22:13:28 +0530

    refactor: reorganize server.js for improved structure and clarity; define jobs array and port earlier

--- server.js
+++ server.js
@@ -1,24 +1,32 @@
 import express from "express";
 import morgan from "morgan";
 import * as dotenv from "dotenv";
+import { nanoid } from "nanoid";
+
 dotenv.config();
 const app = express();
+const port = process.env.PORT || 5100;
+
+let jobs = [
+  { id: nanoid(), company: "Apple", position: "front-end" },
+  { id: nanoid(), company: "google", position: "backend" },
+];
 
 app.use(express.json());
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
-const port = process.env.PORT || 5100;
-
 app.get("/", (req, res) => {
   res.send("Hello world!");
 });
 
-app.post("/", (req, res) => {
-  console.log(req);
-  res.json({ message: "Data Recieved", data: req.body });
+app.get("/api/v1/jobs", (req, res) => {
+  res.status(200).json({ jobs });
 });
+
+
+
 
 app.listen(port, () => {
   console.log(`server is running on port ${port} `);
