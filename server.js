949f0fe75658b14ea8dec489949b6b2c6d78be69
sundareshrx7483 <sundareshrx7483@gmail.com>
2025-07-24 14:00:13 +0530

    feat: implement CRUD operations for jobs in the API

--- server.js
+++ server.js
@@ -25,6 +25,58 @@ app.get("/api/v1/jobs", (req, res) => {
   res.status(200).json({ jobs });
 });
 
+app.get("/api/v1/jobs/:id", (req, res) => {
+  const { id } = req.params;
+  const job = jobs.find((job) => job.id === id);
+  if (!job) {
+    return res
+      .status(400)
+      .json({ msg: `the job for the id:${id} is not found` });
+  }
+  res.status(200).json({ job });
+});
+
+app.post("/api/v1/jobs", (req, res) => {
+  const { company, position } = req.body;
+  if (!company || !position) {
+    return res.status(400).json({ msg: "please provide company & position" });
+  }
+  const id = nanoid(10);
+  const job = { id, company, position };
+  jobs.push(job);
+  res.status(201).json({ job });
+});
+
+app.patch("/api/v1/jobs/:id", (req, res) => {
+  const { company, position } = req.body;
+  if ((!company, !position)) {
+    return res.status(400).json({ msg: "please provide company & position" });
+  }
+  const { id } = req.params;
+  const job = jobs.find((job) => job.id === id);
+  if (!job) {
+    return res
+      .status(400)
+      .json({ msg: `the job for the id:${id} is not found` });
+  }
+  job.company = company;
+  job.position = position;
+  res.status(200).json({ job });
+});
+app.delete("/api/v1/jobs/:id", (req, res) => {
+  const { id } = req.params;
+  const job = jobs.find((job) => job.id === id);
+  if (!job) {
+    return res
+      .status(400)
+      .json({ msg: `the job for the id:${id} is not found` });
+  }
+
+  const newJobs = jobs.filter((job) => job.id === id);
+  jobs = newJobs;
+  res.status(200).json("Job Deleted");
+});
+
 
 
 
