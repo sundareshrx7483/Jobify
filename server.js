1d695eaa70883aec573c51ea819ff9588ac59e61
sundareshrx7483 <sundareshrx7483@gmail.com>
2025-07-28 20:46:38 +0530

    feat: implement job model and update job controller for MongoDB integration; refactor server.js for database connection

--- server.js
+++ server.js
@@ -3,13 +3,14 @@ import morgan from "morgan";
 import * as dotenv from "dotenv";
 dotenv.config();
 import jobRouter from "./routes/jobRouter.js";
+import mongoose from "mongoose";
 
 const app = express();
 app.use(express.json());
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
-const port = process.env.PORT || 5100;
+const port = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.send("Hello world!");
@@ -26,6 +27,13 @@ app.use((err, req, res, next) => {
   res.status(500).json({ msg: "something went wrong!!!" });
 });
 
-app.listen(port, () => {
-  console.log(`server is running on port ${port} `);
-});
+try {
+  await mongoose.connect(process.env.MONGO_URL);
+  app.listen(port, () => {
+    console.log(`server is running on port ${port} `);
+  });
+} catch (error) {
+  console.log(error);
+  process.exit(1);
+}
+
