97eae1790e6484fa4b879dfd33caa6eb4045502f
sundareshrx7483 <sundareshrx7483@gmail.com>
2025-07-30 23:12:43 +0530

    fix: correct typo in errorHandlerMiddleware for statusCode variable

--- middlewares/errorHandlerMiddleware.js
+++ middlewares/errorHandlerMiddleware.js
@@ -2,9 +2,9 @@ import { StatusCodes } from "http-status-codes";
 
 const errorHandlerMiddleware = (err, req, res, next) => {
   console.log(err);
-  const statuScode = err.statuScode || StatusCodes.INTERNAL_SERVER_ERROR;
+  const statusCode = err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR;
   const msg = err.message || "something went wrong!!!, try again later";
-  res.status(statuScode).json({ msg });
+  res.status(statusCode).json({ msg });
 };
 
 export default errorHandlerMiddleware;
