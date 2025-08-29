2f90fd37a688e1fda7342615e17a94656ed2931f
sundareshrx7483 <sundareshrx7483@gmail.com>
2025-08-29 19:55:40 +0530

    implemented proxy in frontend

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import "./modern-theme.css";
 import App from './App.jsx'
-
+fetch("/api/v1/test")
+  .then((res) => res.json())
+  .then((data) => console.log(data));
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
--- server.js
+++ server.js
@@ -19,7 +19,9 @@ if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 const port = process.env.PORT || 5000;
-
+app.get("/api/v1/test", (req, res) => {
+  res.json({ msg: "test route" });
+});
 app.use("/api/v1/jobs", authenticateUser, jobRouter);
 app.use("/api/v1/users", authenticateUser, userRouter);
 app.use("/api/v1/auth", authRouter);
